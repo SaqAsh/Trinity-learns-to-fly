ba3f22fc6d7d19c80c306c969c0bda2a45522ade
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-09-28 13:04:30 -0400

    cooked this up

--- trinity.c
+++ trinity.c
@@ -1,36 +1,70 @@
+#include <netdb.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s hostname\n", argv[0]);
+    return -1;
+  }
+  printf("Connecting to %s on port 80...\n", argv[1]);
 
-int main( int argc, char** argv ) {
-    if ( argc != 2 ) {
-        printf( "Usage: %s hostname\n", argv[0] );
-        return -1;
-    }
-    printf("Connecting to %s on port 80...\n", argv[1]);
+  // lookup server address
+  struct addrinfo hints = {.ai_family = AF_INET, .ai_socktype = SOCK_STREAM};
+  struct addrinfo *server;
+  getaddrinfo(argv[1], "80", &hints, &server);
+  printf("getaddrinfo completed\n");
 
-    // lookup server address
-    struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM };
-    struct addrinfo *server;
-    getaddrinfo(argv[1], "80", &hints, &server);
+  // create socket
+  int sockfd =
+      socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+  printf("socket created\n");
 
-    // create socket
-    int sockfd;
+  // connect to server
+  connect(sockfd, server->ai_addr, server->ai_addrlen);
+  printf("connected to server\n");
 
-    // connect to server
+  // read file contents into buffer
+  char buf[1024];
+  FILE *file = fopen("request.txt", "r");
+  fread(buf, 1, 1024, file);
+  fclose(file);
+  printf("file read completed\n");
 
-    // read file contents into buffer
+  // send buffer to server
+  send(sockfd, buf, 1024, 0);
+  printf("data sent to server\n");
 
-    // send buffer to server
-
-    // receive response and dump to screen
-    
-    freeaddrinfo(server);
+  // receive response and dump to screen
+  const size_t bufSize = 10 * 1024 * 1024;
+  uint8_t *buf_2 = malloc(bufSize);
+  if (buf_2 == NULL) {
+    printf("Failed to allocate memory\n");
     close(sockfd);
+    freeaddrinfo(server);
+    return -1;
+  }
 
-    return 0;
+  size_t rcvd = 0, space = bufSize;
+
+  do {
+    ssize_t bytes = recv(sockfd, buf_2 + rcvd, space, 0);
+    if (bytes <= 0)
+      break;
+    rcvd += bytes;
+    space -= bytes;
+  } while (space > 0);
+
+  // dump response to screen
+  write(1, buf_2, rcvd);
+
+  free(buf_2);
+  freeaddrinfo(server);
+  close(sockfd);
+
+  return 0;
 }
